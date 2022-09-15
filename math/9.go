a21d991929c4ab8eab34dc33a311b789983c5af4
Yuri Gurgenidze <yurigurgenidze@gmail.com>
2022-09-16 01:58:32 +0400

    improve 9.go implementation

--- math/9.go
+++ math/9.go
@@ -1,31 +1,16 @@
 package math
 
-import "math"
-
 func isPalindrome(x int) bool {
-	if x < 0 {
-		return false
-	}
 	return x == reverse(x)
 }
 
 func reverse(x int) int {
-	digits := digitCount(x)
-	reversed := 0
-	for 0 <= digits {
-		reversed += (x % 10) * (int(math.Pow(10, float64(digits))))
-		digits--
+	rev := 0
+
+	for x >= 0 {
+		rev = rev*10 + x%10
 		x /= 10
 	}
 
-	return reversed
-}
-
-func digitCount(x int) int {
-	count := 0
-	for x != 0 {
-		x /= 10
-		count += 1
-	}
-	return count - 1
+	return rev
 }
